1f963a0bd24ef5ff73708a24b52782dbd5f1ebc3
Anders Windell <anders.windell@gmail.com>
2014-01-21 20:39:10 -0700

    Changed name of file

---
+++ Compiler/src/Compiler/mp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compiler;
+
+/**
+ *
+ * @author Anders
+ */
+public class mp {
+    
+    public static void main(){
+        
+    }
+}
