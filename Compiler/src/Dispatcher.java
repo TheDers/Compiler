7f4303789758eea67fdbf6b5553e3e468b9e97d9
Anders Windell <anders.windell@gmail.com>
2014-01-21 20:09:00 -0700

    Added mass faggotry
    Edited some text

--- Compiler/src/Dispatcher.java
+++ Compiler/src/Dispatcher.java
@@ -5,6 +5,6 @@ public class Dispatcher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Compiler is done, A+ plz");
+        System.out.println("Compiler is done, A+ plz, Faggots plz");
     }
 }
