115ad266265d053568ac87e2e214c7a22f032edf
odinkruse <odinkruse@gmail.com>
2014-02-11 22:08:41 -0700

    please work, nonterminals

--- Compiler/src/Compiler/Parser.java
+++ Compiler/src/Compiler/Parser.java
@@ -22,13 +22,8 @@ class Parser {
 
     private void parserMethod(ArrayList tokenList, int tokenCounter) {
         String token = nextToken(tokenList, tokenCounter);
-        switch (token) {
-            case 
-                 {
+        
 
-                }
-
-        }
 
     }
 
@@ -38,399 +33,247 @@ class Parser {
         return nextToken;
     }
 
-    public void expression(String in_token) {
-        if(in_token==)
-            
-    }
-
-    public void expression_tail() {
+    public void system_Goal() {
 
     }
 
-    public void term() {
+    public void program() {
 
     }
 
-    public void term_tail() {
+    public void program_Heading() {
 
     }
 
-    public void factor() {
+    public void block() {
 
     }
 
-    public void factor_tail() {
+    public void variable_Declaration_Part() {
 
     }
 
-    public void primary() {
+    public void variable_Declaration_Tail() {
 
+    }
+    public void variable_Declaration()
+    {
+        
+    }
+    public void type()
+    {
+        
+    }
+    public void procedure_And_Function_Declaration_Part()
+    {
+        
+    }
+    public void procedure_Declaration()
+    {
+        
+    }
+    public void function_Declaration()
+    {
+        
+    }
+    public void procedure_Heading()
+    {
+        
+    }
+    public void function_Heading()
+    {
+        
+    }
+    public void optional_Formal_Parameter_List()
+    {
+        
+    }
+    public void formal_Parameter_Section_Tail()
+    {
+        
+    }
+    public void value_Parameter_Section()
+    {
+        
+    }
+    public void variable_Parameter_Section()
+    {
+        
+    }
+    public void statement_Part()
+    {
+        
+    }
+    public void statement()
+    {
+        
+    }
+    public void empty_Statement()
+    {
+        
+    }
+    public void read_Statement()
+    {
+        
+    }
+    public void read_Parameter_Tail()
+    {
+        
+    }
+    public void read_Parameter()
+    {
+        
+    }
+    public void write_Statement()
+    {
+        
+    }
+    public void write_Parameter_Tail()
+    {
+        
+    }
+    public void write_Parameter()
+    {
+        
+    }
+    public void assignment_Statement()
+    {
+        
+    }
+    public void if_Statement()
+    {
+        
+    }
+    public void optional_Else_Part()
+    {
+        
+    }
+    public void repeat_Statement()
+    {
+        
+    }
+    public void while_Statement()
+    {
+        
+    }
+    public void for_Statement()
+    {
+        
+    }
+    public void control_Variable()
+    {
+        
+    }
+    public void initial_Value()
+    {
+        
+    }
+    public void step_Value()
+    {
+        
+    }
+    public void final_Value()
+    {
+        
+    }
+    public void procedure_Statement()
+    {
+        
+    }
+    public void optional_Actual_Paramenter_List()
+    {
+        
+    }
+    public void actual_Parameter_Tail()
+    {
+        
+    }
+    public void actual_Parameter()
+    {
+        
+    }
+    public void expression()
+    {
+        
+    }
+    public void optional_Relational_Part()
+    {
+        
+    }
+    public void relational_Operator()
+    {
+        
+    }
+    public void simple_Expression()
+    {
+        
+    }
+    public void term_Tail()
+    {
+        
+    }
+    public void optional_Sign()
+    {
+        
+    }
+    public void adding_Operator()
+    {
+        
+    }
+    public void term()
+    {
+        
+    }
+    public void factor_Tail()
+    {
+        
+    }
+    public void multiplying_Operator()
+    {
+        
+    }
+    public void factor()
+    {
+        
+    }
+    public void program_Identifier()
+    {
+        
+    }
+    public void variable_Identifier()
+    {
+        
+    }
+    public void procedure_Identifier()
+    {
+        
+    }
+    public void function_Identifier()
+    {
+        
+    }
+    public void boolean_Expression()
+    {
+        
+    }
+    public void ordinal_Expression()
+    {
+        
+    }
+    public void identifier_List()
+    {
+        
+    }
+    public void indentifier_Tail()
+    {
+        
+    }
+    public void syntax_Error()
+    {
+        
+    }
+    public void match()
+    {
+        
     }
 }
-
-/*
- case "MP_AND":
- //mp_and();
- break;
- case "MP_OR":
- //mp_or();
- break;
- case "MP_BEGIN":
- //mp_begin();
- break;
- case "MP_DIV":
- //mp_div();
- break;
- case "MP_DO":
- //mp_do();
- break;
- case "MP_DOWNTO":
- //mp_downto();
- break;
- case "MP_ELSE":
- //mp_else();
- break;
- case "MP_END":
- //mp_end();
- break;
- case "MP_FALSE":
- //mp_false();
- break;
- case "MP_FIXED":
- //mp_fixed();
- break;
- case "MP_FLOAT":
- //mp_float();
- break;
- case "MP_FOR":
- //mp_for();
- break;
- case "MP_FUNCTION":
- //mp_function();
- break;
- case "MP_IF":
- //mp_if();
- break;
- case "MP_INTEGER":
- //mp_integer();
- break;
- case "MP_MOD":
- //mp_mod();
- break;
- case "MP_NOT":
- //mp_not();
- break;
- case "MP_PROCEDURE":
- //mp_procedure();
- break;
- case "MP_PROGRAM":
- //mp_program();
- break;
- case "MP_READ":
- //mp_read();
- break;
- case "MP_REPEAT":
- //mp_repeat();
- break;
- case "MP_STRING":
- //mp_string();
- break;
- case "MP_THEN":
- //mp_then();
- break;
- case "MP_TRUE":
- //mp_true();
- break;
- case "MP_TO":
- //mp_to();
- break;
- case "MP_UNTIL":
- //mp_until();
- break;
- case "MP_VAR":
- //mp_var();
- break;
- case "MP_WHILE":
- //mp_while();
- break;
- case "MP_WRITE":
- //mp_write();
- break;
- case "MP_WRITELN":
- //mp_writeln();
- break;
- case "Letter":
- //letter();
- break;
- case "digit":
- //digit();
- break;
- case "MP_INTEGER_LIT":
- //mp_integer_lit();
- break;
- case "MP_FIXED_LIT":
- //mp_fixed_lit();
- break;
- case "MP_FLOAT_LIT":
- //mp_float_lit();
- break;
- case "MP_ASSIGN":
- //mp_assign();
- break;
- case "MP_COLON":
- //mp_colon();
- break;
- case "MP_COMMA":
- //mp_comma();
- break;
- case "MP_EQUAL":
- //mp_equal();
- break;
- case "MP_FLOAT_DIVIDE":
- //mp_float_divide();
- break;
- case "MP_GEQUAL":
- //mp_gequal();
- break;
- case "MP_LEQUAL":
- //mp_lequal();
- break;
- case "MP_GTHAN":
- //mp_gthan();
- break;
- case "MP_LTHAN":
- //mp_lthan();
- break;
- case "MP_MINUS":
- //mp_minus();
- break;
- case "MP_NEQUAL":
- //mp_nequal();
- break;
- case "MP_PERIOD":
- //mp_period();
- break;
- case "MP_PLUS":
- //mp_plus();
- break;
- case "MP_RPAREN":
- //mp_rparen();
- break;
- case "MP_LPAREN":
- //mp_lparen();
- break;
- case "MP_SCOLON":
- //mp_scolon();
- break;
- case "MP_TIMES":
- //mp_times();
- break;**/
-/**
- * private void mp_and() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_or() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_times() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_begin() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_div() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_do() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_downto() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_else() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_end() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_false() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_fixed() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_float() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_for() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_function() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_if() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_integer() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_mod() { // throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_not() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_procedure() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_program() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_read() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_repeat() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_string() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_then() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_true() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_to() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_until() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_var() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_while() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_write() { // throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_writeln() { // throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void letter() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void digit() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_integer_lit() { //throw new
- * UnsupportedOperationException("Not supported yet."); //To change body of
- * generated methods, choose Tools | Templates. }
- *
- * private void mp_fixed_lit() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_float_lit() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_assign() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_colon() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_comma() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_equal() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_float_divide() { //throw new
- * UnsupportedOperationException("Not supported yet."); //To change body of
- * generated methods, choose Tools | Templates. }
- *
- * private void mp_gequal() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_lequal() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_gthan() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_lthan() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_lparen() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_minus() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_nequal() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_period() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_plus() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_rparen() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- *
- * private void mp_scolon() { //throw new UnsupportedOperationException("Not
- * supported yet."); //To change body of generated methods, choose Tools |
- * Templates. }
- */
