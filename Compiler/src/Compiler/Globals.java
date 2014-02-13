39e7bd35d99b6bd00d80d611bdd44ae1bbd8ae82
Anders Windell <anders.windell@gmail.com>
2014-02-13 15:39:19 -0700

    Update to Parser.java
    roughly 1/5 of the way done, just updating for now.  Also added the
    Globals.java class which we will use instead of passing values through
    the entire time

---
+++ Compiler/src/Compiler/Globals.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compiler;
+
+/**
+ *
+ * @author Anders
+ */
+public class Globals {
+    public static int tokenCounter=0;
+    public static String token;
+}
--- Compiler/src/Compiler/Parser.java
+++ Compiler/src/Compiler/Parser.java
@@ -12,89 +12,237 @@ import java.util.ArrayList;
  * @author DasRobotos
  */
 class Parser {
+    
+    ArrayList<String> tList;
+    boolean syntax_error = false;
 
     Parser(ArrayList<String> tokenList) {
-        ArrayList<String> tList = tokenList;
-        int tokenCounter = 0;
-        parserMethod(tList, tokenCounter);
+        tList= tokenList;
+        parserMethod(tList, Globals.tokenCounter);
 
     }
 
     private void parserMethod(ArrayList tokenList, int tokenCounter) {
-        String token = nextToken(tokenList, tokenCounter);
-        
+        //String token = nextToken(tokenList, tokenCounter);
+        Globals.token = tokenList.get(tokenCounter).toString();
+        system_Goal();
 
 
     }
 
-    private String nextToken(ArrayList tokenList, int tokenCounter) {
+    private void nextToken(ArrayList tokenList, int tokenCounter) {
         String nextToken = tokenList.get(tokenCounter + 1).toString();
-
-        return nextToken;
+        Globals.tokenCounter++;
+        Globals.token = nextToken;
     }
 
     public void system_Goal() {
-
+        program();
+        if(syntax_error == false){
+            EOF();
+        }
     }
 
     public void program() {
-
+        program_Heading();
+        if(Globals.token.equals("MP_SCOLON")){
+            match(Globals.token, "MP_SCOLON");
+        }else{
+            syntax_Error();
+        }
+        block();
+        if(Globals.token.equals("MP_PERIOD")){
+            match(Globals.token, "MP_PERIOD");
+        }else{
+            syntax_Error();
+        }
     }
 
     public void program_Heading() {
-
+        if(Globals.token.equals("MP_PROGRAM")){     
+            match(Globals.token, "MP_PROGRAM");
+        }else{
+            syntax_Error();
+        }
+        program_Identifier();
     }
 
     public void block() {
-
+        variable_Declaration_Part();
+        procedure_And_Function_Declaration_Part();
+        statement_Part();
     }
 
     public void variable_Declaration_Part() {
-
+        if(Globals.token.equals("MP_VAR")){
+            match(Globals.token, "MP_VAR");
+            variable_Declaration();
+            if(Globals.token.equals("MP_SCOLON")){
+                match(Globals.token, "MP_SCOLON");
+            }else{
+                syntax_Error();
+            }
+            variable_Declaration_Tail();
+        }else if(Globals.token.equals("MP_BEGIN") || Globals.token.equals("MP_FUNCTION") || Globals.token.equals("MP_PROCEDURE")){
+            //epsilon
+        }else{
+            syntax_Error();
+        }
     }
 
     public void variable_Declaration_Tail() {
-
+        if(Globals.token.equals("MP_IDENTIFIER")){
+            variable_Declaration();
+            if(Globals.token.equals("MP_SCOLON")){
+                match(Globals.token, "MP_SCOLON");
+            }else{
+                syntax_Error();
+            }
+            variable_Declaration_Tail();
+        }else if(Globals.token.equals("MP_BEGIN") || Globals.token.equals("MP_FUCTION") || Globals.token.equals("MP_PROCEDURE")){
+            //epsilon
+        }else{
+            syntax_Error();
+        }
     }
     public void variable_Declaration()
     {
-        
+        identifier_List();
+        if(Globals.token.equals("MP_COLON")){
+            match(Globals.token, "MP_COLON");
+        }else{
+            syntax_Error();
+        }
+        type();
     }
     public void type()
     {
-        
+        if(Globals.token.equals("MP_INTEGER")){
+            match(Globals.token, "MP_INTEGER");
+        }else if(Globals.token.equals("MP_FLOAT")){
+            match(Globals.token, "MP_FLOAT");
+        }else if(Globals.token.equals("MP_STRING")){
+            match(Globals.token, "MP_STRING");
+        }else if(Globals.token.equals("MP_BOOLEAN")){
+            match(Globals.token, "MP_BOOLEAN");
+        }else{
+            syntax_Error();
+        }
     }
     public void procedure_And_Function_Declaration_Part()
     {
-        
+        if(Globals.token.equals("MP_PROCEDURE")){           //Rule 14
+            procedure_Declaration();
+            procedure_And_Function_Declaration_Part();
+        }else if(Globals.token.equals("MP_FUNCTION")){      //Rule 15
+            function_Declaration();
+            procedure_And_Function_Declaration_Part();
+        }else if(Globals.token.equals("MP_BEGIN")){         //Rule 16
+            //epsilon
+        }else{
+            syntax_Error();
+        }
     }
     public void procedure_Declaration()
     {
-        
+        procedure_Heading();
+        if(Globals.token.equals("MP_SCOLON")){
+            match(Globals.token, "MP_SCOLON");
+        }else{
+            syntax_Error();
+        }
+        block();
+        if(Globals.token.equals("MP_SCOLON")){
+            match(Globals.token, "MP_SCOLON");
+        }else{
+            syntax_Error();
+        }
     }
     public void function_Declaration()
     {
-        
+        function_Heading();
+        if(Globals.token.equals("MP_SCOLON")){
+            match(Globals.token, "MP_SCOLON");
+        }else{
+            syntax_Error();
+        }
+        block();
+        if(Globals.token.equals("MP_SCOLON")){
+            match(Globals.token, "MP_SCOLON");
+        }else{
+            syntax_Error();
+        }
     }
     public void procedure_Heading()
     {
-        
+        if(Globals.token.equals("MP_PROCEDURE")){
+            match(Globals.token, "MP_PROCEDURE");
+            procedure_Identifier();
+            optional_Formal_Parameter_List();
+        }else{
+            syntax_Error();
+        }
     }
     public void function_Heading()
     {
-        
+        if(Globals.token.equals("MP_FUNCTION")){
+            match(Globals.token, "MP_FUNCTION");
+            function_Identifier();
+            optional_Formal_Parameter_List();
+            type();
+        }else{
+            syntax_Error();
+        }
     }
     public void optional_Formal_Parameter_List()
     {
-        
+        if(Globals.token.equals("MP_LPAREN")){      //Rule 21
+            match(Globals.token, "MP_LPAREN");
+            formal_Parameter_Section();
+            formal_Parameter_Section_Tail();
+            if(Globals.token.equals("MP_RPAREN")){
+                match(Globals.token, "MP_RPAREN");
+            }else{
+                syntax_Error();
+            }
+        }else if(Globals.token.equals("MP_BEGIN") || Globals.token.equals("MP_BOOLEAN") || Globals.token.equals("MP_FLOAT") || Globals.token.equals("MP_FUNCTION") || Globals.token.equals("MP_INTEGER") || Globals.token.equals("MP_PROCEDURE") || Globals.token.equals("MP_STRING")){
+            //Rule 22
+            //epsilon
+        }else{
+            syntax_Error();
+        }
     }
     public void formal_Parameter_Section_Tail()
     {
-        
+        if(Globals.token.equals("MP_SCOLON")){          //Rule 23
+            match(Globals.token, "MP_SCOLON");
+            formal_Parameter_Section();
+            formal_Parameter_Section_Tail();
+        }else if(Globals.token.equals("MP_RPAREN")){    //Rule 24
+            //epsilon
+        }else{
+            syntax_Error();
+        }
+    }
+    public void formal_Parameter_Section()
+    {
+        if(Globals.token.equals("MP_IDENTIFIER")){      //Rule 25
+            value_Parameter_Section();
+        }else if(Globals.token.equals("MP_VAR")){       //Rule 26
+            variable_Parameter_Section();
+        }else{
+            syntax_Error();
+        }
     }
     public void value_Parameter_Section()
     {
-        
+        identifier_List();
+        if(Globals.token.equals("MP_COLON")){
+            match(Globals.token, "MP_COLON");
+        }else{
+            syntax_Error();
+        }
+        type();
     }
     public void variable_Parameter_Section()
     {
@@ -238,19 +386,35 @@ class Parser {
     }
     public void program_Identifier()
     {
-        
+        if(Globals.token.equals("MP_IDENTIFIER")){
+            match(Globals.token, "MP_IDENTIFIER");
+        }else{
+            syntax_Error();
+        }        
     }
     public void variable_Identifier()
     {
-        
+        if(Globals.token.equals("MP_IDENTIFIER")){
+            match(Globals.token, "MP_IDENTIFIER");
+        }else{
+            syntax_Error();
+        }
     }
     public void procedure_Identifier()
     {
-        
+        if(Globals.token.equals("MP_IDENTIFIER")){
+            match(Globals.token, "MP_IDENTIFIER");
+        }else{
+            syntax_Error();
+        }
     }
     public void function_Identifier()
     {
-        
+        if(Globals.token.equals("MP_IDENTIFIER")){
+            match(Globals.token, "MP_IDENTIFIER");
+        }else{
+            syntax_Error();
+        }
     }
     public void boolean_Expression()
     {
@@ -268,12 +432,28 @@ class Parser {
     {
         
     }
+    public void EOF(){
+        System.out.println("The input program parses!");
+    }
     public void syntax_Error()
     {
-        
+        syntax_error = true;
+        try{
+        throw new SyntaxErrorException();
+        }catch (SyntaxErrorException e){
+            System.out.println("Syntax Error");
+        }
     }
-    public void match()
+    public void match(String token, String realToken)
     {
-        
+        if(token.equals(realToken)){
+            nextToken(tList, Globals.tokenCounter);
+        }else{
+            syntax_Error();
+        }
+    }
+    public class SyntaxErrorException extends Exception {
+        public SyntaxErrorException(){
+        }
     }
 }
